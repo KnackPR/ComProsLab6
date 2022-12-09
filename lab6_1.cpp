230a531f2571225705c6c12035e04c86620c102b
KnackPR <peerawich_r@cmu.ac.th>
2022-12-09 15:32:16 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+    int num = 1 ;
+    int even = 0 ;
+    int odd = 0 ;
+    
+    while(num != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num ;
+    if(num != 0){
+         if(num%2==0){
+            even++ ;
+        }else{
+            odd++;
+        }
+    }
+    }
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
